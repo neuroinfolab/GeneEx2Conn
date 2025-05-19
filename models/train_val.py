c9e7902b45fd7e1852228d0a0233bce5a736ec2f
alexander-ratzan <asratzan@gmail.com>
2025-05-19 16:35:01 -0400

    late neurips pushes

--- models/train_val.py
+++ models/train_val.py
@@ -41,12 +41,14 @@ def train_model(model, train_loader, val_loader, epochs, criterion, optimizer, p
                 predictions, targets = model.predict(val_loader)
                 pearson_corr = pearsonr(predictions, targets)[0]
                 
+                '''
                 # Save model if it has cls_init attribute
                 if hasattr(model, 'cls_init'):
                     save_path = os.path.join('models', 'saved_models')
                     os.makedirs(save_path, exist_ok=True)
                     torch.save(model.state_dict(), os.path.join(save_path, 'best_cls_model_seed42fold1.pt'))
                     print(f"Saved best model to {os.path.join(save_path, 'best_cls_model_seed42fold1.pt')}")
+                '''
 
                 if patience_counter >= patience:
                     print(f"\nEarly stopping triggered at epoch {epoch+1}. Restoring best model with Val Loss: {best_val_loss:.4f}, Pearson Correlation: {pearson_corr:.4f}")
